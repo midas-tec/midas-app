8ebb14fe0954e2f3bf1b3fb88ce01405b7cf1c67
Amit Jadhav <amitsjadhav@gmail.com>
2023-01-26 10:47:55 +0100

    initial check in

---
+++ src/main/java/com/midas/app/MidasAppApplication.java
@@ -0,0 +1,13 @@
+package com.midas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidasAppApplication.class, args);
+	}
+
+}
